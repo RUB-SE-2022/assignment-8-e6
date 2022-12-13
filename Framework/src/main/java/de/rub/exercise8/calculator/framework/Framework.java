1acd4337d39e7c13cc33a2e09bb514a9c1a745a2
azraanimating <azraanimating@gmail.com>
2022-12-13 10:29:35 +0100

    Added comments

--- Framework/src/main/java/de/rub/exercise8/calculator/framework/Framework.java
+++ Framework/src/main/java/de/rub/exercise8/calculator/framework/Framework.java
@@ -16,6 +16,13 @@ public class Framework {
     private final Shell shell;
     private final Text text;
     private final ArrayList<Button> buttons;
+
+    /**
+     * This constructor Initializes the Framework (Display, Shell and Text).
+     * It is used to easily create UIs in the form we were Tasked to do.
+     * @param title String that represents the title of the Window
+     * @param baseText The default text that is shown in the entry-field
+     */
     public Framework(String title, String baseText) {
         this.display = new Display();
         this.shell = new Shell(display);
@@ -23,10 +30,17 @@ public class Framework {
         this.buttons = new ArrayList<>();
 
         shell.setText(title);
+        // Here we're defining the default size of our Window
         text.setLayoutData(new RowData(300, SWT.DEFAULT));
         text.setText(baseText);
     }
 
+    /**
+     * This method, as its name might let you guess, enables the User to easily create a Button
+     * to the Right of the textfield
+     * @param text String representing the Name of the button (Its the String on the Button the user sees)
+     * @return the button Object itself
+     */
     public Button addButton(String text) {
         Button button = new Button(shell, 0);
         button.setText(text);
@@ -34,6 +48,9 @@ public class Framework {
         return button;
     }
 
+    /**
+     * This Method renders the User Interface and closes it, if it's signaled to close.
+     */
     public void renderUI() {
         shell.setLayout(new RowLayout());
         this.shell.pack();
--- plugins/Calculator/src/main/java/de/rub/exercise8/calculator/Calculator.java
+++ plugins/Calculator/src/main/java/de/rub/exercise8/calculator/Calculator.java
@@ -5,9 +5,10 @@ import de.rub.exercise8.calculator.framework.Framework;
 public class Calculator {
 
     public Calculator() {
+        // Here we're Just creating an Instance of our Framework
         Framework framework = new Framework("My Great Calculator", "10/2+6");
-        framework.addButton("calculate");
-        framework.renderUI();
+        framework.addButton("calculate"); // This, like the method name says, just creates a button.
+        framework.renderUI(); //this displays our UI.
     }
 
 }
